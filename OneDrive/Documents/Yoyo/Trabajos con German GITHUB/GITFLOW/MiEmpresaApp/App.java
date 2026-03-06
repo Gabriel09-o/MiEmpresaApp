5f118f9494fbc670f2919215e24726f2151eedb0
Julieth <pjimin66771106@gmail.com>
2026-03-06 12:21:35 -0500

    ADD sumar y restar

--- OneDrive/Documents/Yoyo/Trabajos con German GITHUB/GITFLOW/MiEmpresaApp/App.java
+++ OneDrive/Documents/Yoyo/Trabajos con German GITHUB/GITFLOW/MiEmpresaApp/App.java
@@ -1,5 +1,8 @@
 public class App {
-    public static void main(String[] args) {
-        System.out.println("HOLA MUNDO");
+    public static void sumar (double a, double b) {
+        return a + b;
+}
+    public static void restar (double a, double b) {
+        return a - b;
     }
 }

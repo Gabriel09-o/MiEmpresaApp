515088b3b959576626c6df3e7eeaa44094b0e78b
Julieth <pjimin66771106@gmail.com>
2026-03-06 12:01:49 -0500

    Descripcion del cambio

--- OneDrive/Documents/Yoyo/Trabajos con German GITHUB/GITFLOW/MiEmpresaApp/App.java
+++ OneDrive/Documents/Yoyo/Trabajos con German GITHUB/GITFLOW/MiEmpresaApp/App.java
@@ -2,4 +2,10 @@ public class App {
     public static void main(String[] args) {
         System.out.println("HOLA MUNDO");
     }
+    public static void holaMundo() {
+        System.out.println("HOLA MUNDO");
+    }
+    public static void holaMundo2() {
+        System.out.println("HOLA MUNDO");
+    }
 }

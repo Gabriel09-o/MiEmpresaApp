4652ee6e30edaed51892dc62885959b821f027ac
Julieth <pjimin66771106@gmail.com>
2026-03-06 12:35:51 -0500

    ADD multiplicar y dividir

--- OneDrive/Documents/Yoyo/Trabajos con German GITHUB/GITFLOW/MiEmpresaApp/App.java
+++ OneDrive/Documents/Yoyo/Trabajos con German GITHUB/GITFLOW/MiEmpresaApp/App.java
@@ -5,4 +5,10 @@ public class App {
     public static void restar (double a, double b) {
         return a - b;
     }
+    public static void multiplicar (double a, double b) {
+        return a * b;
+    }
+    public static void dividir (double a, double b) {
+        return a / b;
+    }
 }

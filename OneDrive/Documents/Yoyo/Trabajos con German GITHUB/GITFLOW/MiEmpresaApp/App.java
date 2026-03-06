9f0553772a2fda3107aeb092ee032919b4fdb5c6
Julieth <pjimin66771106@gmail.com>
2026-03-06 12:31:32 -0500

    Corrigiendo código y agregando aritmética

--- OneDrive/Documents/Yoyo/Trabajos con German GITHUB/GITFLOW/MiEmpresaApp/App.java
+++ OneDrive/Documents/Yoyo/Trabajos con German GITHUB/GITFLOW/MiEmpresaApp/App.java
@@ -5,10 +5,4 @@ public class App {
     public static void restar (double a, double b) {
         return a - b;
     }
-    public static void holaMundo() {
-        System.out.println("HOLA MUNDO");
-    }
-    public static void holaMundo2() {
-        System.out.println("HOLA MUNDO");
-    }
 }
